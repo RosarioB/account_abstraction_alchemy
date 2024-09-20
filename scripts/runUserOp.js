8902788a6fceaa8603e5b35a4df87666391a8190
Rosario Borgesi <borgesiros@gmail.com>
2024-09-20 11:17:09 +0200

    updated readme

--- scripts/runUserOp.js
+++ scripts/runUserOp.js
@@ -1,8 +1,8 @@
 const hre = require("hardhat");
 
 const FACTORY_NONCE = 1;
-const FACTORY_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
-const ENTRY_POINT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const FACTORY_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+const ENTRY_POINT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 async function main() {
   const entryPoint = await hre.ethers.getContractAt(
